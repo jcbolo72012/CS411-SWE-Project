116be34093d5443d6a7e0ceeacc33e758ce21c0f
specificlanguage <specificlanguage@gmail.com>
2022-04-28 01:26:37 -0400

    Fix auth workflow pt 2
    Also update ReadMe to add all the new parameters required

--- frontend/src/Login.js
+++ frontend/src/Login.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import {Redirect} from "react-router-dom";
-import {useQuery} from "react-query";
+import {Navigate} from "react-router-dom";
+import {useQuery, useQueryClient} from "react-query";
 
 export function Login(){
-
+    const client = useQueryClient()
     const random_str = generateRandomHex(10)
 
     /**
@@ -12,33 +12,46 @@ export function Login(){
      *  {client_id: client_id,
      *   state: state}
      */
-    const {isLoading, data, error} = useQuery(["setString"], async () => {
-        fetch("http://localhost:8000/start_auth/", {
-            method: "POST",
-            body: JSON.stringify({
-                state: random_str
-            })
-        })
-    })
 
-    const link = "https://todoist.com/oauth/authorize?client_id=" + data.client_id + "&scope=data:read_write&state=" + random_str
+    var formdata = new FormData();
+    formdata.append("state", random_str);
+
+    var requestOptions = {
+      method: 'POST',
+      body: formdata,
+      redirect: 'follow', headers: {
+        "Content-Type": "application/x-www-form-urlencoded"
+        }
+    };
+
+    const {isLoading, isError, data, error} = useQuery(["setString"], async () => {
+        console.log(random_str)
+        let response = await fetch("http://localhost:8000/start_auth", requestOptions)
+        return response.json()
+    }, { refetchOnWindowFocus: false, retry: false })
+
+
 
     if(isLoading){
         return <h3>Redirecting...</h3>
-    } else if (error) {
+    } if (isError) {
         return <div><h3>Something wrong happened.</h3> <p>{error}</p></div>
+    } if (data) {
+        const link = "https://todoist.com/oauth/authorize?client_id=" + data.client_id +
+            "&scope=data:read_write&state=" + data.state
+        // return (<Navigate to={link}/>)
+        window.location.href = link;
+        return null;
+
     } else {
-        return (
-            <Redirect to={link}/>
-        )
+        return null;
     }
 }
 
 export function Logout(){
     localStorage.removeItem("token")
-    return <Redirect to="/"/>
+    return <Navigate to="/"/>
 }
-
 
 const generateRandomHex = (length) => {
     const randomNumber = Math.random().toString(16);
